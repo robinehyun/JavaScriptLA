d83171fb39524867d19cf2256679e67faba4cbe5
Robin Yun <robinehyun@gmail.com>
2016-06-30 22:51:37 -0700

    completed 90% of mobile responsive

--- public/js/app.js
+++ public/js/app.js
@@ -29,52 +29,75 @@ $(document).ready(function(){
       }, 500);
   });
 
-//cd-user-modal needs to show on click.
-var modal = $('.cd-user-modal'),
-    register = $('#register'),
-    login = $('#signin'),
-    loginForm = $('#cd-login'),
-    signupForm = $('#cd-signup'),
-    forgotPassword = $('.forgot-password'),
-    loginFromRegister = $('.register-login'),
-    closeForm = $('.cd-close-form');
-//if register is clicked, the register modal should show
-register.click(function(e){
-  e.preventDefault();
-  modal.toggleClass('is-visible');
-  loginForm.toggleClass('hide');
-});
+  //cd-user-modal needs to show on click.
+  var modal = $('.cd-user-modal'),
+      register = $('#register'),
+      login = $('#signin'),
+      loginForm = $('#cd-login'),
+      signupForm = $('#cd-signup'),
+      forgotPassword = $('.forgot-password'),
+      loginFromRegister = $('.register-login'),
+      closeForm = $('.cd-close-form');
+  //if register is clicked, the register modal should show
+  register.click(function(e){
+    e.preventDefault();
+    modal.toggleClass('is-visible');
+    loginForm.toggleClass('hide');
+  });
 
-//if login is clicked OR login is clicked on the register modal, login modal should show
-login.click(function(e){
-  e.preventDefault();
-  modal.toggleClass('is-visible');
-  signupForm.toggleClass('hide');
-});
+  //if login is clicked OR login is clicked on the register modal, login modal should show
+  login.click(function(e){
+    e.preventDefault();
+    modal.toggleClass('is-visible');
+    signupForm.toggleClass('hide');
+  });
 
-//when close button is clicked, modal should disappear
-closeForm.click(function(e){
-  e.preventDefault();
-  modal.removeClass('is-visible');
-  if(loginForm.hasClass('hide') === true){
-    setTimeout(function(){
-      loginForm.removeClass('hide');
-    },400);
-  } else if (signupForm.hasClass('hide') === true){
-    setTimeout(function(){
-      signupForm.removeClass('hide');
-    },400);
-  } else {
-    loginForm.addClass('hide');
-    signupForm.addClass('hide');
-  }
-});
-
-
-$(document).keyup(function(e){
-    if(e.which=='27'){
-      modal.removeClass('is-visible');
+  //when close button is clicked, modal should disappear
+  closeForm.click(function(e){
+    e.preventDefault();
+    modal.removeClass('is-visible');
+    if(loginForm.hasClass('hide') === true){
+      setTimeout(function(){
+        loginForm.removeClass('hide');
+      },600);
+    } else if (signupForm.hasClass('hide') === true){
+      setTimeout(function(){
+        signupForm.removeClass('hide');
+      },600);
+    } else {
+      loginForm.addClass('hide');
+      signupForm.addClass('hide');
     }
+  });
+
+  //close modal when esc key is pressed
+  $(document).keyup(function(e){
+      if(e.which=='27'){
+        modal.removeClass('is-visible');
+        setTimeout(function(){
+          signupForm.removeClass('hide');
+          loginForm.removeClass('hide');
+        },600);
+      }
+    });
+
+  //Mobile nav
+  $(".cross").hide();
+  $(".menu").hide();
+  $(".hamburger").click(function() {
+    $(".menu").slideToggle("slow", function() {
+      $(".hamburger").hide();
+      $(".cross").show();
+      //$(".btn-header").css("border-bottom", "#1d1f20 1px solid");
+    });
+  });
+
+  $(".cross").click(function() {
+    $(".menu").slideToggle("slow", function() {
+      $(".cross").hide();
+      $(".hamburger").show();
+      //$(".btn-header").css("border-bottom", "none");
+    });
   });
 
 });
